028b6e0324998f703c96ec2f2b92375f52b8cc9f
jake-sexton <42213544+jake-sexton@users.noreply.github.com>
2025-02-17 12:11:25 +0000

    [Jira-101] - Added 'buying a product' end-to-end test (#1)
    * added new test in example.spec.ts
    
    * adding .only for demo
    
    * applying pull request feedback to example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,13 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('Jakes test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
